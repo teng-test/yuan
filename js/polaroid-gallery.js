d5068c6937a28bb0c147ca4e8190c235b43616f3
teng-eb <lvteng@easternbay.cn>
2020-02-14 18:44:41 +0800

    yuan-3: Update file

--- js/polaroid-gallery.js
+++ js/polaroid-gallery.js
@@ -91,7 +91,7 @@ var polaroidGallery = (function () {
                 if (currentItem) {
                     select(currentItem);
                 }
-            }, 100);
+            }, 500);
         });
     }
 
